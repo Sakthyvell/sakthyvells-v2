8c640e32ce304c68242cf0b2de5a1514216c81fb
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2022-01-24 22:06:18 +0800

    Creating views and templates for blog-detail page

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,19 @@
 from pipes import Template
 from pyexpat import model
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from .models import Article, Category
 
 
-class BlogListingView(TemplateView):
+class BlogListingView(ListView):
     template_name = 'blog-listing.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Article.objects.all()
-        context['categories'] = Category.objects.all()
-        return context
+    model = Article
+    context_object_name = 'posts'
 
 class BlogDetailView(DetailView):
     model = Article
     template_name = 'blog-detail.html'
 
-    
+
+# class BlogCategoryListView(ListView):
+#     template_name = 'blog-listing.html'
+
