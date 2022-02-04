a5873591ffe58ef2a5e71d160a0206721cf1e6b0
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2022-02-04 21:00:34 +0800

    Adding error propagation in heroku logs

--- sakthyvells/settings.py
+++ sakthyvells/settings.py
@@ -33,6 +33,9 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
+# To get the relevant errors to propagate up through the Heroku logs
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 
@@ -156,3 +159,37 @@ CKEDITOR_CONFIGS = {
             ]
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
