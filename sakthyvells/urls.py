82386d5626b674a3b23fb2088c88ddb182fef5bd
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2022-03-06 15:24:30 +0800

    Changing admin url

--- sakthyvells/urls.py
+++ sakthyvells/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('my-site-login-2712/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
