1394cb35857c51163a08cb735827df91c9b37aad
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2022-03-06 20:05:54 +0800

    media url fix

--- sakthyvells/settings.py
+++ sakthyvells/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # CKEditor Configs
