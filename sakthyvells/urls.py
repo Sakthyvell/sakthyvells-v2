85264cd051f0cd6ca6fcdd97d2d7bbc30577db0e
Sakthyvell <sakthyvellsuperamaniam@gmail.com>
2022-03-06 19:52:37 +0800

    Ckeditor image uploader url

--- sakthyvells/urls.py
+++ sakthyvells/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('my-site-login-2712/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
